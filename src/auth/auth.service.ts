033c5bcc2b2714c0ce338e170b6d994116202562
Sasha Maslak <95244349+SashaMaslak@users.noreply.github.com>
2024-08-22 10:38:08 +0300

    completed backend

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,94 @@
+import {
+	BadRequestException,
+	Injectable,
+	NotFoundException,
+	UnauthorizedException
+} from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { UserService } from 'src/user/user.service'
+import { AuthDto } from './dto/auth.dto'
+import { verify } from 'argon2'
+import { Response } from 'express'
+
+@Injectable()
+export class AuthService {
+	EXPIRE_DAY_REFRESH_TOKEN = 1
+	REFRESH_TOKEN_NAME = 'refreshToken'
+
+	constructor(
+		private jwt: JwtService,
+		private userService: UserService
+	) {}
+
+	async login(dto: AuthDto) {
+		const { password, ...user } = await this.validateUser(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return { user, ...tokens }
+	}
+
+	async register(dto: AuthDto) {
+		const isOldUser = await this.userService.getByEmail(dto.email)
+		if (isOldUser) throw new BadRequestException('User already exists')
+
+		const { password, ...user } = await this.userService.create(dto)
+		const tokens = this.issueTokens(user.id)
+
+		return { user, ...tokens }
+	}
+
+	private issueTokens(userId: string) {
+		const data = { id: userId }
+
+		const accessToken = this.jwt.sign(data, { expiresIn: '1h' })
+
+		const refreshToken = this.jwt.sign(data, { expiresIn: '1h' })
+
+		return { accessToken, refreshToken }
+	}
+
+	private async validateUser(dto: AuthDto) {
+		const user = await this.userService.getByEmail(dto.email)
+		if (!user) throw new NotFoundException('User not found')
+
+		const isValid = await verify(user.password, dto.password)
+		if (!isValid) throw new NotFoundException('Invalid password')
+
+		return user
+	}
+
+	async getNewTokens(refreshToken: string) {
+		const result = await this.jwt.verifyAsync(refreshToken)
+		if (!result) throw new UnauthorizedException('Invalid refresh token')
+
+		const { password, ...user } = await this.userService.getById(result.id)
+		const tokens = this.issueTokens(user.id)
+
+		return { user, ...tokens }
+	}
+
+	addRefreshTokenToResponse(res: Response, refreshToken: string) {
+		const expiresIn = new Date()
+		expiresIn.setDate(expiresIn.getDate() + this.EXPIRE_DAY_REFRESH_TOKEN)
+
+		res.cookie(this.REFRESH_TOKEN_NAME, refreshToken, {
+			httpOnly: true,
+			domain: 'localhost',
+			expires: expiresIn,
+			secure: true,
+			// lax if production
+			sameSite: 'none'
+		})
+	}
+
+	removeRefreshTokenToResponse(res: Response) {
+		res.cookie(this.REFRESH_TOKEN_NAME, '', {
+			httpOnly: true,
+			domain: 'localhost',
+			expires: new Date(0),
+			secure: true,
+			// lax if production
+			sameSite: 'none'
+		})
+	}
+}
